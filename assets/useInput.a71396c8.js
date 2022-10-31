96b8d9912faadf950165aea051d0508b9218d290
KooAme <dg.changeworld@gmail.com>
2022-10-31 23:57:18 +0900

    two

---
+++ assets/useInput.a71396c8.js
@@ -0,0 +1,1 @@
+import{a}from"./index.cde5efb9.js";function f(u,e){const[r,n]=a.exports.useState(u),s=t=>{n(typeof t=="string"?t:()=>t.target.value)};return e?{value:r,onChange:s,label:e}:{value:r,onChange:s}}export{f as u};
---
+++ assets/useModal.3835343a.js
@@ -0,0 +1,1 @@
+import{a as n}from"./index.cde5efb9.js";function r(){const[o,e]=n.exports.useState(!1);return{open:o,onOpen:()=>{e(!0)},onClose:()=>{e(!1)}}}export{r as u};
