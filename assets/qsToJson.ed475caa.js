20d958b455b09ac7dd290568af56b33632e943c0
KooAme <dg.changeworld@gmail.com>
2022-10-31 22:22:42 +0900

    first commit

---
+++ assets/qsToJson.ed475caa.js
@@ -0,0 +1,1 @@
+function i(s){const t=s.slice(1).split("&");if(!t[0])return;const o=t.map(n=>n.split("="));return Object.fromEntries(o)}export{i as q};
---
+++ assets/useInput.c1267fa4.js
@@ -0,0 +1,1 @@
+import{a}from"./index.e9cc1e82.js";function f(u,e){const[r,n]=a.exports.useState(u),s=t=>{n(typeof t=="string"?t:()=>t.target.value)};return e?{value:r,onChange:s,label:e}:{value:r,onChange:s}}export{f as u};
